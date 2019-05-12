2ac5a014dedb3a760d0ad40bcff70fd007372fa7
Osa Julian <osajulian@hotmail.com>
2019-05-11 20:51:41 -0400

    introduction to components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'pm-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+selector: 'pm-root',
+template: `<div><h1>{{pageTitle}}</h1>
+<div>Numero Uno Component</div>
+</div>`
 })
 export class AppComponent {
-  title = 'Angular: Getting Started';
+  pageTitle: string = 'Solomon Grundy Product Management.';
 }
